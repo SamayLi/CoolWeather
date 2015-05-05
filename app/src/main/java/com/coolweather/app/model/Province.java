8c13136c5e00668cf54cccbd9142f0c6e0611a09
samay <samay0904@163.com>
2015-05-05 21:48:46 +0800

    添加对应的实体类

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,34 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by dysis on 2015/5/5.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
